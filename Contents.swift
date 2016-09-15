8176714f7ec3a9fbe37d64efb19809783a88b58a
kmart22 <kmar7070@740s-pl-111.local>
2016-09-15 09:35:07 -0600

    my first playground in swift

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "kaden Martinsen"
+var number = 123456
+print(number + 12)
+print("\(number)" + str)
+let dog = "🐕"; print(dog)
